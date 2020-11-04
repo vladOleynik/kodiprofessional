e0b476058a696c954e8e381dc3f3110d6e57653d
Trikas <trikaka12@gmail.com>
2020-11-04 22:15:32 +0200

    ADD: гугл тег менеджер
    Took 11 minutes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -176,6 +176,14 @@
 <meta name="google-site-verification" content="JOqLN92S-8qTq7ZLWWMjOJIr0Xrzg_E-SyixuR-MoLQ" />
 </head>
 <body>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MFHKT9V');
+</script>
+<!-- End Google Tag Manager -->
 <!-- NavMenus start-->
 <div class="menu-button-mobile">
     <div class="menu-icon">
