5b04ad83f8bade542307361d166b1d8d12b53f0c
Олейник Влад <trikaka12@gmail.com>
2021-02-02 23:26:06 +0200

    ADD: free space

--- app/Http/Controllers/PersonalCabinet/IndexController.php
+++ app/Http/Controllers/PersonalCabinet/IndexController.php
@@ -18,11 +18,17 @@ class IndexController extends Controller
     public function index()
     {
         //подгружаем у вложенного отношения несколько отношений
-        $orders = Auth::user()->shopOrders()->with(['status', 'details.product'=>function($query){
-            $query->with(['categories','meta']);
+        $orders = Auth::user()->shopOrders()->with(['status', 'details.product' => function ($query) {
+            $query->with(['categories', 'meta']);
         }])->get();
-      //загружаем урлы для формирования ссылок на продукты
+        //загружаем урлы для формирования ссылок на продукты
         $url = \App\Helpers\Catalog\Categories::all()['urls'];
         return view('personal-cabinet', compact('orders', 'url'));
+    }
+
+    public function size()
+    {
+        $df = round(disk_free_space("/") / 1024 / 1024 / 1024);
+        print("Free space: $df GB");
     }
 }
